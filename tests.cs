e94ff50b8b1a3eb180e885fa51e142e408e85036
Shimoda <kuri65536@hotmail.com>
2018-07-11 08:32:23 +0900

    tests for the command line parser.

--- tests.cs
+++ tests.cs
@@ -57,6 +57,91 @@ public class PrePandocTest {
         Assert.AreEqual(na, 2, "attributes");
         Assert.AreEqual(nc, 11, "data");  // actual result.
     }
+
+    /// <remarks>
+    /// </remarks>
+    [Test]
+    public void test_options() {
+        string droot, ftop, fout;
+        var test = new string[] {};
+        PrePandoc.Options.run(test, out droot, out ftop, out fout);
+        Assert.AreEqual(".", droot, "droot");
+        Assert.AreEqual("source.md", ftop, "ftop");
+        Assert.AreEqual("temp.md", fout, "fout");
+    }
+
+    /// <remarks>
+    /// </remarks>
+    [Test]
+    public void test_options_no() {
+        string droot, ftop, fout;
+        var test = new string[] {};
+        PrePandoc.Options.run(test, out droot, out ftop, out fout);
+        Assert.AreEqual(".", droot, "droot");
+        Assert.AreEqual("source.md", ftop, "ftop");
+        Assert.AreEqual("temp.md", fout, "fout");
+    }
+
+    /// <remarks>
+    /// </remarks>
+    [Test]
+    public void test_options_args() {
+        string droot, ftop, fout;
+        var test = new[] {"..", "source2.md", "temp2.md"};
+        PrePandoc.Options.run(test, out droot, out ftop, out fout);
+        Assert.AreEqual("..", droot, "droot");
+        Assert.AreEqual("source2.md", ftop, "ftop");
+        Assert.AreEqual("temp2.md", fout, "fout");
+    }
+
+    /// <remarks>
+    /// </remarks>
+    [Test]
+    public void test_options_opts() {
+        string droot, ftop, fout;
+        var test = new[] {"...", "-b", "source3.md", "-o", "temp3.md",
+                          "-v", "20", "--empty-block=yes",
+                          "--article-tags", "param,summary"};
+        PrePandoc.Options.run(test, out droot, out ftop, out fout);
+        Assert.AreEqual("...", droot, "droot");
+        Assert.AreEqual("source3.md", ftop, "ftop");
+        Assert.AreEqual("temp3.md", fout, "fout");
+        // int
+        Assert.AreEqual(20, PrePandoc.logging.__level__, "level");
+        // bool
+        Assert.AreEqual(true, PrePandoc.Config.f_output_empty_block, "empty");
+        // string[]
+        Assert.AreEqual(new[] {"param", "summary"},
+                        PrePandoc.Config.tags_article, "tags");
+    }
+
+    /// <remarks>
+    /// </remarks>
+    [Test]
+    public void test_options_ng1() {
+        string droot, ftop, fout;
+        var test = new[] {"-v", "a"};
+        var ret = PrePandoc.Options.run(test, out droot, out ftop, out fout);
+        Assert.AreEqual(true, ret, "ng result");
+    }
+
+    /// <remarks>
+    /// </remarks>
+    [Test]
+    public void test_options_ng2() {
+        string droot, ftop, fout;
+        var test = new[] {"", "-b", "", "-o", "",
+                          "--empty-block=nogo", "--output-tags", ""};
+        PrePandoc.Options.run(test, out droot, out ftop, out fout);
+        Assert.AreEqual(".", droot, "droot");
+        Assert.AreEqual("source.md", ftop, "ftop");
+        Assert.AreEqual("temp.md", fout, "fout");
+        // bool => default
+        Assert.AreEqual(false, PrePandoc.Config.f_output_empty_block, "empty");
+        // string[] => default
+        Assert.AreEqual(new[] {"remarks"},
+                        PrePandoc.Config.tags_output, "tags");
+    }
 }
 }
 // vi: ft=cs:sw=4:ts=4:et:nowrap:fdm=marker
